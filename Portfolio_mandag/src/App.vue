ae64af3c6aa6ee340bf502f7cec44270ca6911cd
Helena Hollænder <helena.hollaender@gmail.com>
2024-03-12 09:42:21 +0100

    Init git

---
+++ Portfolio_mandag/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+      <nav class="grid grid-cols-3 underline">
+        <RouterLink to="/"><img src="./components/Logo.png" alt="Logo" class="w-[200px] pl-4"></RouterLink>
+        <RouterLink to="/about">about</RouterLink>
+        <RouterLink to="/contact">contact</RouterLink>
+      </nav>
+   
+  </header>
+
+  <RouterView></RouterView>
+</template>
+
