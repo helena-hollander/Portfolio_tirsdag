7c070bc9a79012f07f12f97dec005a2be3efadf0
helena-hollander <helena.hollander@gmail.com>
2024-03-13 15:27:41 +0100

    onsdag, igen

--- Portfolio_mandag/src/views/projects/Project1View.vue
+++ Portfolio_mandag/src/views/projects/Project1View.vue
@@ -4,7 +4,7 @@
             <img src="../../assets/Resting_grey_web.jpg" class="absolute h-[30vh] translate-y-[10vh] translate-x-[13vw]">
         </div>
       <div class=""> <!--OBS, bruger flex... gider egentlig ikke-->
-        <img src="../../assets/Resting_web.jpg" class="absolute h-[90vw] translate-y-[5vh] translate-x-[30vw]">
+        <img src="../../assets/Resting_web.jpg" class=" h-[90vw] translate-y-[5vh] translate-x-[30vw]">
       </div>
     </body>
   </template>
